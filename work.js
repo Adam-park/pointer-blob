528c3686759a340628101630782ba925ec5162b4
Adam-park <zmdkdkt@gmail.com>
2026-08-17 20:45:47 +0900

    사이트 내용물 초기화, 새 프로젝트 준비 (깃허브/Vercel 연결 유지)

--- work.js
+++
@@ -1,7 +0,0 @@
-// Work 페이지: 카테고리 구분 없이 전체 작업을 한 그리드에 노출
-const grid = document.getElementById("workGrid");
-
-if (grid) {
-  works.forEach((w) => grid.appendChild(createWorkCard(w)));
-  attachTooltip(grid);
-}
